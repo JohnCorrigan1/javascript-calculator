e71c322d6b00272bf1d461fa302334963f925c75
John C <jcorrigan322@gmail.com>
2022-05-03 20:06:25 -0400

    intial commit with basic js funtions

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+add(1, 3);
+subtract(10, 2);
+multiply(3, 5);
+divide(10, 2);
